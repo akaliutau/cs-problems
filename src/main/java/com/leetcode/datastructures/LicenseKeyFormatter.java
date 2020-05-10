96f72dd9dbe244156c37b624399240852adb5f67
Aliaksei Kaliutau <k5771k@gmail.com>
2020-05-10 17:29:19 +0000

    contest 188

---
+++ src/main/java/com/leetcode/datastructures/LicenseKeyFormatter.java
@@ -0,0 +1,128 @@
+package com.leetcode.datastructures;
+
+/**
+ * https://leetcode.com/problems/license-key-formatting/
+ * 
+ * You are given a license key represented as a string S which consists only
+ * alphanumeric character and dashes. The string is separated into N+1 groups by
+ * N dashes.
+ * 
+ * Given a number K, we would want to reformat the strings such that each group
+ * contains exactly K characters, except for the first group which could be
+ * shorter than K, but still must contain at least one character. Furthermore,
+ * there must be a dash inserted between two groups and all lowercase letters
+ * should be converted to uppercase.
+ * 
+ * Given a non-empty string S and a number K, format the string according to the
+ * rules described above.
+ * 
+ * Example 1: Input: S = "5F3Z-2e-9-w", K = 4
+ * 
+ * Output: "5F3Z-2E9W"
+ * 
+ * Explanation: The string S has been split into two parts, each part has 4
+ * characters. Note that the two extra dashes are not needed and can be removed.
+ * Example 2: Input: S = "2-5g-3-J", K = 2
+ * 
+ * Output: "2-5G-3J"
+ * 
+ * Explanation: The string S has been split into three parts, each part has 2
+ * characters except the first part as it could be shorter as mentioned above.
+ * Note: The length of string S will not exceed 12,000, and K is a positive
+ * integer. String S consists only of alphanumerical characters (a-z and/or A-Z
+ * and/or 0-9) and dashes(-). String S is non-empty
+ * 
+ * 
+ */
+public class LicenseKeyFormatter {
+	
+	static class LicenseKey {
+		public String s;
+		public int idx;
+		public int symbols = 0;
+		public int groups = 0;
+		public int firstGroupLen = 0;
+		public int len = 0;
+		
+		public LicenseKey(String s, int len) {
+			this.s = s.toLowerCase();
+			this.len = len;
+		}
+		
+		public char next() {
+			while ( idx < s.length()) {
+				char c= s.charAt(idx);
+				idx ++;
+				if (c != '-') {
+					if (c >= '0' && c <= '9') {
+						return c;
+					}
+					int index = c - 'a';
+					return (char)(index + 'A');
+				}
+			}
+			return 0;
+		}
+
+		
+		public void analyse() {
+			for (int i = 0; i < s.length(); i++) {
+				if (s.charAt(i) != '-') {
+					symbols ++;
+				}
+			}
+			if (symbols % len != 0) {
+				groups = symbols / len + 1;
+				firstGroupLen = symbols % len;
+			}else {
+				groups = symbols / len;
+				firstGroupLen = len;
+				if (groups == 0) {
+					groups = 1;
+				}
+			}
+		}
+		
+		public String getGroup(int length) {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < length; i++) {
+				sb.append(next());
+			}
+			return sb.toString();
+		}
+		
+		public String getReformatted() {
+			String[] str = new String[groups];
+			str[0] = getGroup(firstGroupLen);
+			for (int i = 1; i < groups; i++) {
+				str[i] = getGroup(len);
+			}
+			return String.join("-", str);
+		}
+
+		@Override
+		public String toString() {
+			return "LicenseKey [s=" + s + ", idx=" + idx + ", symbols=" + symbols + ", groups=" + groups
+					+ ", firstGroupLen=" + firstGroupLen + ", len=" + len + "]";
+		}
+		
+	}
+
+	public static String licenseKeyFormatting(String s, int k) {
+		LicenseKey key = new LicenseKey(s,k);
+		key.analyse();
+		if (key.symbols == 0) {
+			return "";
+		}
+		return key.getReformatted();
+	}
+
+	public static void main(String[] arg) {
+
+		System.out.println(licenseKeyFormatting("5F3Z-2e-9-w",4));
+		System.out.println(licenseKeyFormatting("2-5g-3-J",2));
+		System.out.println(licenseKeyFormatting("---",2));
+
+	}
+
+}
---
+++ src/main/java/com/leetcode/topology/TrappingRainWaterII.java
@@ -0,0 +1,158 @@
+package com.leetcode.topology;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/trapping-rain-water/
+ * 
+ * Given n non-negative integers representing an elevation map where the width
+ * of each bar is 1, compute how much water it is able to trap after raining. 
+ * 
+ * 
+ * Example:
+ * 
+ * Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 
+ * Output: 6
+ * 
+ * 
+ */
+public class TrappingRainWaterII {
+	
+	public static class Extr {
+		public boolean type;// true - max, false - min
+		public int h;
+		public int pos;
+		
+		public Extr(int h, int pos, boolean type) {
+			this.h = h;
+			this.type = type;
+			this.pos = pos;
+		}
+		
+		@Override
+		public String toString() {
+			return "[h="+h+",pos="+pos+"]";
+		}
+	}
+
+	public static int trap(int[] height) {
+		int vol = 0;
+		int lastMaxHeight = 0;
+		if (height.length < 3) {
+			return 0;
+		}
+
+		boolean up = false;
+		int absMax = 0;
+		List<Extr> maxs = new ArrayList<>();
+		
+		maxs.add(new Extr(height[0],0,true));
+
+		for (int i = 1; i < height.length; i++) {
+			int h = height[i];
+			int diff = h - height[i - 1];
+
+
+			if ((diff < 0) && up) {// max
+				up = false;
+				maxs.add(new Extr(height[i - 1],i,true));
+				lastMaxHeight = height[i - 1];
+				absMax = absMax < lastMaxHeight ? lastMaxHeight : absMax;
+			}
+			if ((diff > 0) && !up) {// min
+				up = true;
+				lastMaxHeight = lastMaxHeight < height[i - 1] ? height[i - 1] : lastMaxHeight;
+			}
+
+		}
+		maxs.add(new Extr(height[height.length-1],height.length-1,true));
+		// check first and last elem
+		if (Math.min(height[0], height[height.length-1]) >= absMax) {
+			maxs = new ArrayList<>();
+			maxs.add(new Extr(height[0],0,true));
+			maxs.add(new Extr(height[height.length-1],height.length-1,true));
+		}
+		
+		Comparator<Extr> sortByHeight = (o, p) -> Integer.compare(p.h, o.h);// asc
+
+		maxs.sort(sortByHeight);
+		System.out.println(maxs);
+
+		// render height map
+
+		Extr lastMax = maxs.get(0);
+		int leftPos = lastMax.pos;
+		int rightPos = lastMax.pos;
+		for (int i = 1; i < maxs.size(); i++) {
+			Extr extr = maxs.get(i);
+			if (leftPos <= extr.pos  && extr.pos <= rightPos) {
+				continue;
+			}
+			int level = Math.min(lastMax.h, extr.h);
+			int l = 0;
+			int r = 0;
+			if (lastMax.pos > extr.pos) {
+				r = lastMax.pos;
+				l = extr.pos;
+			}else {
+				l = lastMax.pos;
+				r = extr.pos;
+			}
+
+			for (int j = l; j < r; j++) {
+				if (leftPos <= j  && j < rightPos) {
+					continue;
+				}
+				int diff = level-height[j];
+				vol += (diff > 0 ? diff : 0);
+			}
+			leftPos = leftPos > l ? l : leftPos;
+			rightPos = rightPos < r ? r : rightPos;
+
+			if (leftPos == 0 && rightPos == height.length-1) {
+				break;
+			}
+		}		
+		
+		return vol;
+
+	}
+
+	public static void main(String[] arg) {
+
+		int[] arr = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+		System.out.println(trap(arr));//6
+		
+		int[] arr1 = { 3, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+		System.out.println(trap(arr1));//14
+		
+		int[] arr2 = { 3, 1, 0, 0, 1, 0, 1 };
+		System.out.println(trap(arr2));
+
+		int[] arr3 = { 3, 1, 0, 2, 1, 0 };
+		System.out.println(trap(arr3));
+
+		int[] arr4 = { 1,1,1};
+		System.out.println(trap(arr4));
+
+		int[] arr5 = { 1,0,1};
+		System.out.println(trap(arr5));
+
+		int[] arr6 = { 6, 5, 0, 4, 0, 1 };
+		System.out.println(trap(arr6));//5
+
+		int[] arr7 = {5,2,1,2,1,5 };
+		System.out.println(trap(arr7));//14
+
+		int[] arr8 = {5,5,1,7,1,1,5,2,7,6 };
+		System.out.println(trap(arr8));//23
+
+		int[] arr9 = {6,4,2,0,3,2,0,3,1,4,5,3,2,7,5,3,0,1,2,1,3,4,6,8,1,3};
+		System.out.println(trap(arr9));//83
+
+	}
+
+}
