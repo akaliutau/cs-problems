142bee64e39c82a276ab70fd59b68746d3eda10c
Aliaksei Kaliutau <k5771k@gmail.com>
2020-05-18 05:44:41 +0000

    added solutions

--- src/main/java/com/leetcode/arrays/MonotonicArray.java
+++ src/main/java/com/leetcode/arrays/MonotonicArray.java
@@ -11,74 +11,56 @@ package com.leetcode.arrays;
  * 
  * Return true if and only if the given array A is monotonic.
  * 
+ * 
+ * 
  * Example 1:
- * Input: [1,2,2,3] Output: true 
  * 
- * Example 2:
- * Input: [6,5,4,4] Output: true 
+ * Input: [1,2,2,3] Output: true
  * 
- * Example 3:
- * Input: [1,3,2] Output: false 
- * 
- * Example 4:
- * Input: [1,2,4,5] Output: true 
- * 
- * Example 5:
- * Input: [1,1,1] Output: true
- * 
- * 
- * Note:
- * 1 <= A.length <= 50000 
- * -100000 <= A[i] <= 100000
  */
 public class MonotonicArray {
-	
-	public static boolean isMonotonic(int[] a) {
-		
-		int n = a.length;
-		if (n == 1) {
-			return true;
-		}
-		boolean increasing = true;
-		for (int i = 1; i < n; i++) {
-			if (a[i] < a[i-1]) {
-				increasing = false;
-				break;
-			}
-		}
-		if (increasing) {
-			return true;
-		}
-		boolean decreasing = true;
-		for (int i = 1; i < n; i++) {
-			if (a[i] > a[i-1]) {
-				decreasing = false;
-				break;
-			}
-		}
-		if (decreasing) {
-			return true;
-		}
-		
-		return false;
 
-	}
+	public static boolean isMonotonic(int[] a) {
+        int n = a.length;
+        if (n <= 1) {
+        	return true;
+        }
+        int diff = 0;
+        for (int i = 0; i < n-1; i++) {
+        	if (a[i] < a[i+1]) {// inc
+        		if (diff < 0) {
+        			return false;
+        		}else {
+        			diff = 1;
+        		}
+        	}else if (a[i] > a[i+1]){//dec
+        		if (diff > 0) {
+        			return false;
+        		}else {
+        			diff = -1;
+        		}
+        	}else {// neutral
+        		
+        	}
+        }
+        return true;
+    }
 
 	public static void main(String[] arg) {
-		
+
 		int[] a = {1,2,2,3};
 		System.out.println(isMonotonic(a));
 
-		int[] a1 = {6,5,4,4};
+		int[] a1 = {1,1,1};
 		System.out.println(isMonotonic(a1));
 
-		int[] a2 = {1,3,2};
+		int[] a2 = {3,2,2,1};
 		System.out.println(isMonotonic(a2));
 
-		int[] a3 = {1,2,4,5};
+		int[] a3 = {3,2,2,3};
 		System.out.println(isMonotonic(a3));
 
-		int[] a4 = {2,2,2};
+		int[] a4 = {3,2};
 		System.out.println(isMonotonic(a4));
 
 	}
